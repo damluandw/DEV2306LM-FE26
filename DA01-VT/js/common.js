aa4a239cc923306ad41eadac818bcd53fafbf806
damluandw <damluan7@gmail.com>
2023-08-13 01:57:30 +0700

    update DA01-VT index v1

--- DA01-VT/js/common.js
+++ DA01-VT/js/common.js
@@ -5,7 +5,15 @@
 //   });
 
 $(document).ready(function () {
-  $(".banner-right").slick({
-
+  $(".img-banner").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    arrows: false
   });
 });
