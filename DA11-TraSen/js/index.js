1a2aef4971d3cd26846010c1eadc631a7b7fb235
damluandw <damluan7@gmail.com>
2023-09-07 23:45:22 +0700

    update DA11-TraSen 1h

---
+++ DA11-TraSen/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".list-product").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    arrows: true,
+  });
+});
+
+
+$(document).ready(function () {
+  $(".list-product .slick-arrow").html("");
+
+});
+
