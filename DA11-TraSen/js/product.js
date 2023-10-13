cb0bf63a66a68d10999c1d8bfb85b4402bb3c6e4
damluandw <damluan7@gmail.com>
2023-10-13 23:57:36 +0700

    update DA-TraSen

--- DA11-TraSen/js/product.js
+++ DA11-TraSen/js/product.js
@@ -1,29 +1,43 @@
-let min = 50000;
-let max = 10000000;
-
-const calcLeftPosition = (value) => value* (min / max) ;
-
-$("#rangeMin").on("input", function (e) {
-  const newValue = parseInt(e.target.value);
-  console.log(newValue);
-  if (newValue > max) return;
-  min = newValue;
-  $("#thumbMin").css("left", calcLeftPosition(newValue) + "%");
-  $("#min").html(newValue);
-  $("#line").css({
-    left: calcLeftPosition(newValue) + "%",
-    right: 100 - calcLeftPosition(max) + "%",
+changPrice();
+function changPrice() {
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 1000000,
+    values: [0, 200000],
+    slide: function (event, ui) {
+      $("#amount").val(" Từ " + ui.values[0] + " đến " + ui.values[1]);
+      // dataFilter = data.filter(
+      //   (x) =>
+      //     x.product.PRICE_NEW >= ui.values[0] &&
+      //     x.product.PRICE_NEW <= ui.values[1]
+      // );
+      // updatePageCount(dataFilter.length, pageSize);
+      // setPagination(dataFilter.length, 1);
+      // setPagingDataHtml(pageSize, 1);
+    },
   });
-});
 
-$("#rangeMax").on("input", function (e) {
-  const newValue = parseInt(e.target.value);
-  if (newValue < min) return;
-  max = newValue;
-  $("#thumbMax").css("left", calcLeftPosition(newValue) + "%");
-  $("#max").html(newValue);
-  $("#line").css({
-    left: calcLeftPosition(min) + "%",
-    right: 100 - calcLeftPosition(newValue) + "%",
-  });
-});
+  $("#amount").val(
+    "Từ " +
+      $("#slider-range").slider("values", 0) +
+      " đến " +
+      $("#slider-range").slider("values", 1)
+  );
+}
+// Format number: 50000 -> 50,000
+// function formatNumber(num) {
+//   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+// }
+// function changeUnit() {
+//   $(document).on("change", 'select[name="khoiluong"]', function () {
+//     let price = $(this).attr("data-price");
+//     let minUnit = $(this).attr("data-unit");
+//     let val = parseInt($(this).val());
+//     let row = ` <p class="m-0">${formatNumber(
+//       price * (val / minUnit)
+//     )}<span>VND</span>  </p>`;
+//     $(".items[data-id=" + $(this).attr("data-id") + "] .price-sp").html(row);
+//   });
+// }
+// changeUnit();
